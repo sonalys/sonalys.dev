cc524ec3166ee6332f980ae444334087835f3ca6
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com.>
2024-01-29 15:38:02 +0100

    implement gatsby + reactjs for static website

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import * as styles from "./index.module.css"
+
+const Header = () => (
+  <header className={styles.header}>
+    {/* <Link to="/" className={styles.menuEntry}>Home</Link> */}
+    {/* <Link to="/" className={styles.menuEntry}>Blog</Link> */}
+  </header>
+)
+
+export default Header
